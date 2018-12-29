06224b974afd8e3d209b9c799bff709da9a92888
Mateusz <matty.szczesny@gmail.com>
2018-12-29 15:07:31 +0100

    add helper for commonly used logic

--- app/src/main/java/pl/politechnika/szczesny/words_world_client/helper/SessionHelper.java
+++ app/src/main/java/pl/politechnika/szczesny/words_world_client/helper/SessionHelper.java
@@ -1,4 +1,19 @@
 package pl.politechnika.szczesny.words_world_client.helper;
 
+import android.app.Application;
+
+import pl.politechnika.szczesny.words_world_client.model.Token;
+
+import static pl.politechnika.szczesny.words_world_client.helper.SharedPrefHelper.getTokenFormSP;
+
 public class SessionHelper {
+    public static boolean isSessionActive(Application application) {
+        Token token = getTokenFormSP(application);
+        return !token.getToken().isEmpty();
+    }
+
+    public static String getToken(Application application) {
+        Token token = getTokenFormSP(application);
+        return token.getToken();
+    }
 }
--- app/src/main/java/pl/politechnika/szczesny/words_world_client/model/Token.java
+++ app/src/main/java/pl/politechnika/szczesny/words_world_client/model/Token.java
@@ -1,4 +1,23 @@
 package pl.politechnika.szczesny.words_world_client.model;
 
-public class Token {
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Token implements Serializable {
+
+    @SerializedName("token")
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return "Token " + token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
