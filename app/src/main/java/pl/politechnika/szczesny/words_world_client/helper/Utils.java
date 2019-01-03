776f5dedd24e8db4ef517989fdcf7f57fad644d0
Mateusz <matty.szczesny@gmail.com>
2019-01-03 23:37:13 +0100

    add method to return flag emoji

--- app/src/main/java/pl/politechnika/szczesny/words_world_client/helper/Utils.java
+++ app/src/main/java/pl/politechnika/szczesny/words_world_client/helper/Utils.java
@@ -5,7 +5,9 @@ import android.graphics.Color;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ConstHelper {
+import pl.politechnika.szczesny.words_world_client.model.Language;
+
+public class Utils {
     static final String TABOO_SCORE__SP = "TABOO_SCORE__SP";
     public static final String GOOGLE_API__KEY = "AIzaSyBmtY8VhmWWpfBOthj3Q728H7tt79-haFs";
     static final String USER__SP = "USER_SP";
@@ -31,4 +33,19 @@ public class ConstHelper {
         put("MEDIUM", 2);
         put("HARD", 3);
     }};
+
+    public static String returnFlagEmojiForLanguage(Language language) {
+        int flagOffset = 0x1F1E6;
+        int asciiOffset = 0x41;
+
+        String country = language.getLanguageCode().toUpperCase();
+
+        if ("EN".equals(country)) country = "GB";
+
+        int firstChar = Character.codePointAt(country, 0) - asciiOffset + flagOffset;
+        int secondChar = Character.codePointAt(country, 1) - asciiOffset + flagOffset;
+
+        return new String(Character.toChars(firstChar))
+                + new String(Character.toChars(secondChar));
+    }
 }
